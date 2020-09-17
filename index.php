98722ee8749482f3e52d7f65d4174298fdfef609
pauline-mfr <p.monfrance@codeur.online>
2020-09-17 09:46:57 +0200

    cards for created sections

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $request->closeCursor();
     <meta charset="utf-8">
     <title>Back end</title>
     <link href="main.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   </head>
   <body>
     <h1>Back-office</h1>
@@ -37,8 +38,6 @@ $request->closeCursor();
               <?= $data['description'] ?>
             </li>
           <?php endforeach; ?>
-          <p>Price</p>
-          <p>Category</p>
         </div>
         <div>
     </div>
@@ -58,18 +57,26 @@ $ids = $request->FetchAll();//Mode(PDO::FETCH_ASSOC);
 $request->closeCursor();
 ?>
 
-<div>
-  <h2>TEST AFFICHAGE</h2>
+<br><h2>TEST AFFICHAGE</h2>
+<div class="card-group">
   <?php foreach ($ids as $id): ?>
-    <li>
-      <?= $id['title'] ?>
-      <?= $id['description'] ?>
-      <?= $id['price'] ?>
-      <?= $id['category'] ?>
-    </li>
-  <?php endforeach; ?>
+    <div class="card" style="width: 10rem;">
+
+    <div class="card-body">
+      <h5 class="card-title"><?= $id['title'] ?></h5>
+       <img src="..." class="card-img-top" alt="...">
+      <p class="card-text"><?= $id['description'] ?></p>
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item"><strong>Price :</strong> <?= $id['price'] ?>€</li>
+      <li class="list-group-item"><strong>Category : </strong><?= $id['category'] ?></li>
+    </ul>
+
+</div>
+</div>
+<?php endforeach; ?>
 </div>
 
-
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
   </body>
 </html>
